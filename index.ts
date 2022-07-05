439eb35be1e9930edbae415e7dd41f986d5bf50a
AntonioYudiMapa <108767212+AntonioYudiMapa@users.noreply.github.com>
2022-07-05 19:41:19 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ let map: google.maps.Map;
 
 function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: -23.558, lng: 46.720 },
     zoom: 8,
   });
 }
