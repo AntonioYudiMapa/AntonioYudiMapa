26b931778e6567b5b6e3f5f09d448a21426c95b8
AntonioYudiMapa <108767212+AntonioYudiMapa@users.noreply.github.com>
2022-07-05 22:51:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -23.558  , lng: -46.720 },
-    zoom: 8, 
+    zoom: 11, 
   });
 }
 
