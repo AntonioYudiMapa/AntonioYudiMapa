c53de37a5ca7675b10f04403baf48e7625d01fb5
AntonioYudiMapa <108767212+AntonioYudiMapa@users.noreply.github.com>
2023-04-17 01:03:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-let map;
+var map;
+var marker;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -23.558  , lng: -46.720 },
-    zoom: 11, 
-  });
-}
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -23.5505, lng: -46.6333}, // São Paulo, Brasil
+        zoom: 14
+    });
 
-window.initMap = initMap;
+    marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP
+    });
+
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            marker.setPosition(pos);
+            map.setCenter(pos);
+        });
+    } else {
+        alert("Seu navegador não suporta geolocalização!");
+    }
+}
